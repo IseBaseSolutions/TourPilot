1c2fb3d9772e67f57c175129f28e8be0a69b9834
andrewgtv <batmangtv@gmail.com>
2013-11-01 11:16:32 +0200

    1

---
+++ TourPilot/src/isebase/cognito/tourpilot/Activity/TestClass.java
@@ -0,0 +1,13 @@
+package isebase.cognito.tourpilot.Activity;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
