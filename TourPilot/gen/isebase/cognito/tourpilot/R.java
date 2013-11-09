02604ff77e4bb52773079c39557b189ac3f19e28
andrewgtv <batmangtv@gmail.com>
2013-11-09 11:36:51 +0200

    error

--- TourPilot/gen/isebase/cognito/tourpilot/R.java
+++ TourPilot/gen/isebase/cognito/tourpilot/R.java
@@ -24,7 +24,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int dialog_password=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_screen_locked_to_landscape=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int action_clear_database=0x7f08001b;
