e3e46ba8cc87ac28f7f1699b67c90fc1ca6ae64f
Bogdan <Bogdan@WINCTRL-DMSKV7I>
2013-11-11 12:05:58 +0200

    add utilizer

---
+++ TourPilot/src/isebase/cognito/tourpilot/Utils/Utilizer.java
@@ -0,0 +1,36 @@
+package isebase.cognito.tourpilot.Utils;
+
+import isebase.cognito.tourpilot.Data.BaseObject.BaseObject;
+import java.util.List;
+
+public class Utilizer {
+
+	public static int[] getIDs(List<? extends BaseObject> items){
+		int[] retVal = new int[items.size()];
+		for(int i = 0; i < items.size();i++)
+			retVal[i] = items.get(i).getId();
+		return retVal;
+	}
+	
+	public static String getIDsString(List<? extends BaseObject> items){
+		String retVal = "";
+		for(int i = 0; i < items.size();i++)
+		{
+			retVal += String.format("%s%s" 
+					, retVal == "" ? "" : ", "
+					, items.get(i).getId());
+		}
+		return retVal;
+	}
+	
+	public static String getIDsString(int[] items){
+		String retVal = "";
+		for(int i = 0; i < items.length;i++)
+		{
+			retVal += String.format("%s%s" 
+					, retVal == "" ? "" : ", "
+					, items[i]);
+		}
+		return retVal;
+	}	
+}
