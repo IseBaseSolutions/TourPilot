66523bb113551947895d9d6ad7d97c5d2e972223
andrewgtv <batmangtv@gmail.com>
2013-11-16 17:07:35 +0200

    1

---
+++ TourPilot/src/isebase/cognito/tourpilot/Data/Employment/Employment.java
@@ -0,0 +1,110 @@
+package isebase.cognito.tourpilot.Data.Employment;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import isebase.cognito.tourpilot.Data.BaseObject.BaseObject;
+import isebase.cognito.tourpilot.Data.Patient.Patient;
+import isebase.cognito.tourpilot.Data.PilotTour.PilotTour;
+import isebase.cognito.tourpilot.Data.Task.Task;
+import isebase.cognito.tourpilot.DataBase.MapField;
+import isebase.cognito.tourpilot.Utils.DateUtils;
+
+public class Employment extends BaseObject {
+	
+	public static final String PatientIDField = "patient_id";
+	public static final String PilotTourIDField = "pilot_tour_id";
+	public static final String TourIDField = "tour_id"; 
+	public static final String DateField = "date";
+
+	private int patientID;	
+	private int pilotTourID;
+	private Date date;
+	private int tourID;
+	
+	private List<Task> tasks =  new ArrayList<Task>();
+	
+	private Patient patient;
+	private PilotTour pilotTour;
+	
+	@MapField(DatabaseField = DateField)
+	public Date getDate() {
+		return date;
+	}
+
+	@MapField(DatabaseField = DateField)
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	@MapField(DatabaseField = TourIDField)
+	public int getTourID() {
+		return tourID;
+	}
+
+	@MapField(DatabaseField = TourIDField)
+	public void setTourID(int id) {
+		this.tourID = id;
+	}
+	
+	@MapField(DatabaseField = PatientIDField)
+	public int getPatientID() {
+		return patientID;
+	}
+
+	@MapField(DatabaseField = PatientIDField)
+	public void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+
+	@MapField(DatabaseField = PilotTourIDField)
+	public int getPilotTourID() {
+		return pilotTourID;
+	}
+
+	@MapField(DatabaseField = PilotTourIDField)
+	public void setPilotTourID(int pilotTourID) {
+		this.pilotTourID = pilotTourID;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public PilotTour getPilotTour() {
+		return pilotTour;
+	}
+
+	public void setPilotTour(PilotTour pilotTour) {
+		this.pilotTour = pilotTour;
+	}
+	
+	public Employment() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return getName() + "  " + DateUtils.HourMinutesFormat.format(getDate());
+	}
+	
+	@Override
+	public String forServer() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+}
---
+++ TourPilot/src/isebase/cognito/tourpilot/Data/Employment/EmploymentManager.java
@@ -0,0 +1,64 @@
+package isebase.cognito.tourpilot.Data.Employment;
+
+import java.util.List;
+import isebase.cognito.tourpilot.Data.BaseObject.BaseObjectManager;
+import isebase.cognito.tourpilot.Data.Patient.PatientManager;
+import isebase.cognito.tourpilot.Data.Task.TaskManager;
+import android.database.sqlite.SQLiteDatabase;
+
+public class EmploymentManager extends BaseObjectManager<Employment> {
+
+	public static final String TableName = "Employments";
+	
+	private static EmploymentManager instance;
+
+	public static EmploymentManager Instance() {
+		if (instance != null)
+			return instance;
+		instance = new EmploymentManager();
+		instance.open();
+		return instance;
+	}
+	
+	public EmploymentManager() {
+		super(Employment.class);
+	}
+
+	@Override
+	public String getRecTableName() {
+		return TableName;
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db) {
+		
+	}
+	
+	public void createEmployments() {
+		clearTable();
+		String strSQL = String.format("SELECT " +
+				"t1.employment_id as _id, " +
+				"t1.patient_id as patient_id, " +
+				"t1.pilot_tour_id as pilot_tour_id, " +
+				"t1.was_sent, " +
+				"t1.is_server_time, " +
+				"t1.checksum, " +
+				"(t2.surname || ', ' || t2.name) as name, " +
+				"t1.plan_date as date, " +
+				"t1.tour_id as tour_id " +
+				"FROM %1$s t1 " +
+				"INNER JOIN %2$s t2 on t1.patient_id = t2._id " +
+				"GROUP BY t1.employment_id"
+				, TaskManager.TableName
+				, PatientManager.TableName
+				, getRecTableName()); 
+		List<Employment> items = load(strSQL);		
+		save(items);
+	}
+	
+	@Override
+	public void afterLoad(List<Employment> items) {
+		super.afterLoad(items);
+	}
+
+}
